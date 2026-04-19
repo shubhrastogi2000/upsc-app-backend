0894d1b796c4820e98b594736f8b75e2b35e6650
shubhrastogi2000 <65946736+shubhrastogi2000@users.noreply.github.com>
2026-04-19 15:42:51 +0530

    fix file

--- app/core/database.py
+++ app/core/database.py
@@ -1,23 +1,24 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.core.config import DATABASE_URL
-from sqlalchemy.orm import Session
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# 🔥 IMPORTANT FIX FOR RENDER
-if DATABASE_URL.startswith("postgres://"):
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
 
-print("DATABASE_URL:", DATABASE_URL)  # Debugging line
+print("DATABASE_URL:", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(
-    autocommit=False, 
-    autoflush=False, 
-    bind=engine)
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
